3e2518784b25f5821fb0946e7faa4a03227978ab
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-02-02 10:57:52 -0300

    Adiciona primeiro commit do repositório, contendo primeiro exercício do guia de JS, sobre variáveis em variaveis/index.js

---
+++ variaveis/index.js
@@ -0,0 +1,16 @@
+// 1) Crie 3 variáveis contendo:
+
+// - Seu nome;
+// - Sua idade;
+// - Comida favorita;
+
+// Após declarar as variáveis e atribuir os valores, utilize o **`console.log`** para formar uma frase como:
+
+// “Meu nome é …, tenho …. anos e minha comida favorita é …..”
+
+const nome = 'Pedro'
+const idade = 22
+const comidaFavorita = 'Pizza'
+
+// console.log('Meu nome é ' + nome + ', tenho ' + idade + ' anos e minha comida favorita é ' + comidaFavorita)
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e minha comida favorita é ${comidaFavorita}`)
