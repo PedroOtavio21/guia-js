bd4deb2211f55a890b75321ad0ceb050a548ef75
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2025-02-02 11:46:16 -0300

    Adiciona solução de exercícios 3 e 4 em sessão de arrays, do guia de JS

---
+++ arrays/ex03.js
@@ -0,0 +1,8 @@
+// 3.Inserindo Elementos no Início do Array:
+// Utilize o método unshift para adicionar dois novos animais no início do array animais.
+
+// Array alterado em último exercício
+let animais = ['gato', 'cavalo']
+
+animais.unshift('vaca', 'galinha')
+console.log(`Array modificado: ${animais.join(' ')}`)
---
+++ arrays/ex04.js
@@ -0,0 +1,9 @@
+// 4. Alterando Elementos Específicos do Array:
+// Mude o segundo elemento do array animais para "girafa".
+
+// Array modificado em exercício anterior
+let animais = ['vaca', 'galinha', 'gato', 'cavalo']
+
+animais[1] = 'girafa'
+console.log(`Segunda posição do Array: ${animais[1]}`)
+console.log(`Array modificado: ${animais.join(' ')}`)
